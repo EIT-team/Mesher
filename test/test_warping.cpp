d4ae5b36df7ce75207b44cfcb343be8dbc65f430
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2020-01-13 11:15:47 +0000

    Fix typos

--- test/test_warping.cpp
+++ test/test_warping.cpp
@@ -2,6 +2,7 @@
 
 #include "deform_volume.h"
 #include "mesh_operations.h"
+#include "input_parameters.h"
 
 #include "write_c3t3_to_vtk_xml_file.h"
 
@@ -10,30 +11,33 @@ using namespace std;
 
 using namespace std;
 
-TEST_CASE("Array indexing") {
+TEST_CASE("Array indexing")
+{
   Deform_Volume warper;
   int dims = 5;
   warper.dims = dims; //Length of x,y,z, dimension
   //Setup
-  int i,j,k;
+  int i, j, k;
   long idx;
 
+  SECTION("Indexing 'normal' values")
+  {
 
-  SECTION("Indexing 'normal' values") {
-
-    for(i = 0; i < dims; i++) {
-      for(j = 0; j < dims; j++) {
-        for(k = 0; k < dims; k++) {
+    for (i = 0; i < dims; i++)
+    {
+      for (j = 0; j < dims; j++)
+      {
+        for (k = 0; k < dims; k++)
+        {
 
           idx = warper.get_array_index(i, j, k);
-          REQUIRE (idx == (i * dims + j) * dims + k);
-
-
+          REQUIRE(idx == (i * dims + j) * dims + k);
         }
       }
     }
 
-    SECTION ("Indexing large dimension array") {
+    SECTION("Indexing large dimension array")
+    {
       // Test for final elment of typical array size
       int big_dim = 512;
       warper.dims = big_dim;
@@ -41,34 +45,33 @@ TEST_CASE("Array indexing") {
 
       idx = warper.get_array_index(max_array_idx, max_array_idx, max_array_idx);
 
-      REQUIRE(idx == big_dim*big_dim*big_dim-1);
+      REQUIRE(idx == big_dim * big_dim * big_dim - 1);
     }
   }
 
-  SECTION("Trying to index values that don't exist") {
+  SECTION("Trying to index values that don't exist")
+  {
 
-    int too_big = dims+1;
-    REQUIRE (warper.get_array_index(too_big, too_big, too_big) == -1);
-    REQUIRE (warper.get_array_index(-1,-1,-1) == -1);
-
+    int too_big = dims + 1;
+    REQUIRE(warper.get_array_index(too_big, too_big, too_big) == -1);
+    REQUIRE(warper.get_array_index(-1, -1, -1) == -1);
   }
 }
 
-
-
-TEST_CASE("Neighbouring elements to first elment") {
+TEST_CASE("Neighbouring elements to first elment")
+{
 
   Deform_Volume warper;
   int dims = 5;
   warper.dims = dims; //Length of x,y,z, dimension
 
   long idx;
-    int expected_neighbours = 7;
+  int expected_neighbours = 7;
 
   // Check element 0
   idx = 0;
 
-  vector<long> neighbours = warper.neighbouring_elements( idx);
+  vector<long> neighbours = warper.neighbouring_elements(idx);
 
   long expected[] = {1, 5, 6, 25, 26, 30, 31};
   vector<long> expected_results(expected, expected + sizeof(expected) / sizeof(long));
@@ -76,13 +79,12 @@ TEST_CASE("Neighbouring elements to first elment") {
   // Sort vector for consistent results
   sort(neighbours.begin(), neighbours.end());
 
-  REQUIRE (neighbours.size() == expected_neighbours);
+  REQUIRE(neighbours.size() == expected_neighbours);
   REQUIRE(neighbours == expected_results);
-
 }
 
-
-TEST_CASE("Neighbouring elements to last corner") {
+TEST_CASE("Neighbouring elements to last corner")
+{
 
   Deform_Volume warper;
   int dims = 5;
@@ -101,14 +103,12 @@ TEST_CASE("Neighbouring elements to last corner") {
   // Sort vector for consistent results
   sort(neighbours.begin(), neighbours.end());
 
-  REQUIRE (neighbours.size() == expected_neighbours);
+  REQUIRE(neighbours.size() == expected_neighbours);
   REQUIRE(neighbours == expected_results);
-
-
 }
 
-
-TEST_CASE("Neighbouring elements to centre") {
+TEST_CASE("Neighbouring elements to centre")
+{
 
   Deform_Volume warper;
   int dims = 5;
@@ -124,89 +124,90 @@ TEST_CASE("Neighbouring elements to centre") {
   // Sort vector for consistent results
   sort(neighbours.begin(), neighbours.end());
 
-  long expected[] = { 31, 32, 33, 36, 37, 38, 41, 42, 43,
-    56, 57, 58, 61, 63, 66, 67, 68,
-    81, 82, 83, 86, 87, 88, 91, 92, 93};
+  long expected[] = {31, 32, 33, 36, 37, 38, 41, 42, 43,
+                     56, 57, 58, 61, 63, 66, 67, 68,
+                     81, 82, 83, 86, 87, 88, 91, 92, 93};
 
-    vector<long> expected_results(expected, expected + sizeof(expected) / sizeof(long));
+  vector<long> expected_results(expected, expected + sizeof(expected) / sizeof(long));
 
-    REQUIRE (neighbours.size() == expected_neighbours);
-    REQUIRE(neighbours == expected_results);
-  }
+  REQUIRE(neighbours.size() == expected_neighbours);
+  REQUIRE(neighbours == expected_results);
+}
 
-
-TEST_CASE ("Dilation") {
+TEST_CASE("Dilation")
+{
 
   Deform_Volume warper;
   int dims = 5;
   warper.dims = dims; //Length of x,y,z, dimension
 
-    int total_elements = dims*dims*dims;
+  int total_elements = dims * dims * dims;
 
-    unsigned char test_input[dims*dims*dims];
-    warper.image_data = test_input;
+  unsigned char test_input[dims * dims * dims];
+  warper.image_data = test_input;
 
-    int i,j,k;
-    int central_element = 62; // centre of 5x5x5
-    long idx;
+  int i, j, k;
+  int central_element = 62; // centre of 5x5x5
+  long idx;
 
-    unsigned char layer_0 = (unsigned char)0;
-    unsigned char layer_1 = (unsigned char)1;
+  unsigned char layer_0 = (unsigned char)0;
+  unsigned char layer_1 = (unsigned char)1;
 
-    for (i = 0; i < total_elements; i++) {
-      test_input[i] = layer_0;
-    }
-
-
-    SECTION ("Dilate central elment") {
-
-      // Set central element to 1
-      test_input[central_element] = layer_1;
-
-      warper.dilate_layer(1, 1);
-
-      // Check some values that should not have changed
-      REQUIRE(test_input[0] == layer_0);
-      REQUIRE(test_input[2] == layer_0);
-      REQUIRE(test_input[dims*dims*dims-1] == layer_0);
-
-      // All these values should have changed
-      long expected[] = { 31, 32, 33, 36, 37, 38, 41, 42, 43,
-        56, 57, 58, 61, 63, 66, 67, 68,
-        81, 82, 83, 86, 87, 88, 91, 92, 93};
-        vector<long> expected_results(expected, expected + sizeof(expected) / sizeof(long));
-
-        long current_idx;
-
-        while (!expected_results.empty()) {
-
-          current_idx = expected_results.back();
-          REQUIRE ( test_input[current_idx] == layer_1);
-          expected_results.pop_back();
-
-        }
-
-      }
-
-    SECTION ("Dilate by 2 pixels") {
-      test_input[central_element] = layer_1;
-
-      // This should set all elements to 1
-      warper.dilate_layer(1, 2);
-
-      // Check all elements are 1
-      for (i = 0; i < total_elements; i++) {
-        REQUIRE( test_input[i] == layer_1);
-      }
-
-    }
-
+  for (i = 0; i < total_elements; i++)
+  {
+    test_input[i] = layer_0;
   }
 
-TEST_CASE ("Identify unique layers in unit cube") {
+  SECTION("Dilate central elment")
+  {
+
+    // Set central element to 1
+    test_input[central_element] = layer_1;
+
+    warper.dilate_layer(1, 1);
+
+    // Check some values that should not have changed
+    REQUIRE(test_input[0] == layer_0);
+    REQUIRE(test_input[2] == layer_0);
+    REQUIRE(test_input[dims * dims * dims - 1] == layer_0);
+
+    // All these values should have changed
+    long expected[] = {31, 32, 33, 36, 37, 38, 41, 42, 43,
+                       56, 57, 58, 61, 63, 66, 67, 68,
+                       81, 82, 83, 86, 87, 88, 91, 92, 93};
+    vector<long> expected_results(expected, expected + sizeof(expected) / sizeof(long));
+
+    long current_idx;
+
+    while (!expected_results.empty())
+    {
+
+      current_idx = expected_results.back();
+      REQUIRE(test_input[current_idx] == layer_1);
+      expected_results.pop_back();
+    }
+  }
+
+  SECTION("Dilate by 2 pixels")
+  {
+    test_input[central_element] = layer_1;
+
+    // This should set all elements to 1
+    warper.dilate_layer(1, 2);
+
+    // Check all elements are 1
+    for (i = 0; i < total_elements; i++)
+    {
+      REQUIRE(test_input[i] == layer_1);
+    }
+  }
+}
+
+TEST_CASE("Identify unique layers in unit cube")
+{
   /* Check that get_layers() function is working correctly */
 
-  const char* inr_path = "../test/unit_cube.inr";
+  const char *inr_path = "../test/unit_cube.inr";
   CGAL::Image_3 image;
   image.read(inr_path);
 
@@ -214,7 +215,7 @@ TEST_CASE ("Identify unique layers in unit cube") {
 
   warper.get_layers();
   std::vector<int> expected = {1};
-  REQUIRE (warper.layers == expected);
+  REQUIRE(warper.layers == expected);
 
   // Change one of the elments to a new layer
   int new_layer = 2;
@@ -222,32 +223,31 @@ TEST_CASE ("Identify unique layers in unit cube") {
   warper.get_layers();
   expected.push_back(new_layer);
 
-  REQUIRE (warper.layers == expected);
+  REQUIRE(warper.layers == expected);
 
   new_layer = 5;
   warper.image_data[7] = new_layer;
   warper.get_layers();
   expected.push_back(new_layer);
 
-  REQUIRE (warper.layers == expected);
-
+  REQUIRE(warper.layers == expected);
 }
 
+TEST_CASE("Deformation distance validation")
+{
 
-TEST_CASE ("Deformation distance validation") {
-
-  double valide = 1.1;
+  double valid = 1.1;
   double invalid = -1;
 
   REQUIRE(check_distances_ok(valid, valid, valid) == 1);
   REQUIRE(check_distances_ok(valid, valid, invalid) == 0);
   REQUIRE(check_distances_ok(valid, invalid, valid) == 0);
   REQUIRE(check_distances_ok(invalid, valid, valid) == 0);
-
 }
 
 //TODO: Use function for loading deformations
-TEST_CASE ("Defined deformations of unit cube") {
+TEST_CASE("Defined deformations of unit cube")
+{
   /* Do some defined (as opposed to random) deformations on a unit cube
    and check the resulting mesh is as expected */
 
@@ -255,55 +255,52 @@ TEST_CASE ("Defined deformations of unit cube") {
 
   cout << "Saving some stretches performed on the unit cube" << endl;
 
-  const char* inr_path = "../test/unit_cube.inr";
+  const char *inr_path = "../test/unit_cube.inr";
 
-      vector< vector<double> > deformations;
+  vector<vector<double>> deformations;
 
-      ifstream deform_file("../test/list_of_deformations.txt", std::ifstream::in);
-      if (!deform_file) perror ("\n Error opening deformations file");
-          cout << "Reading deformations from fie." << endl;
+  ifstream deform_file("../test/list_of_deformations.txt", std::ifstream::in);
+  if (!deform_file)
+    perror("\n Error opening deformations file");
+  cout << "Reading deformations from fie." << endl;
 
-            double dist_x, dist_y, dist_z;
+  double dist_x, dist_y, dist_z;
 
-            while(deform_file >> dist_x >> dist_y >> dist_z)
-            {
-                    cout << dist_x << " " << dist_y << " " << dist_z <<endl;
+  while (deform_file >> dist_x >> dist_y >> dist_z)
+  {
+    cout << dist_x << " " << dist_y << " " << dist_z << endl;
 
-                    vector<double> this_deform;
-                    this_deform.push_back(dist_x);
-                    this_deform.push_back(dist_y);
-                    this_deform.push_back(dist_z);
+    vector<double> this_deform;
+    this_deform.push_back(dist_x);
+    this_deform.push_back(dist_y);
+    this_deform.push_back(dist_z);
 
-                    deformations.push_back(this_deform);
+    deformations.push_back(this_deform);
+  }
 
-            }
+  int i;
+  for (i = 0; i < deformations.size(); i++)
+  {
 
-      int i;
-      for(i = 0; i < deformations.size(); i++) {
+    CGAL::Image_3 image;
+    image.read(inr_path);
 
-        CGAL::Image_3 image;
-        image.read(inr_path);
+    Deform_Volume warper(&image);
 
-        Deform_Volume warper(&image);
+    warper.defined_stretch(deformations[i]);
 
-        warper.defined_stretch(deformations[i]);
+    cout << "Creating mesh" << endl;
 
-        cout << "Creating mesh" << endl;
+    Mesh_domain domain(image);
+    Mesh_criteria criteria(facet_angle = 30, facet_size = 0.03, facet_distance = 1, cell_radius_edge_ratio = 3, cell_size = 0.03);
 
-        Mesh_domain domain(image);
-        Mesh_criteria criteria(facet_angle=30, facet_size=0.03, facet_distance=1, cell_radius_edge_ratio=3, cell_size=0.03);
+    C3t3_EIT c3t3;
+    c3t3 = CGAL::make_mesh_3<C3t3_EIT>(domain, criteria, CGAL::parameters::features(domain),
+                                       CGAL::parameters::no_lloyd(), CGAL::parameters::no_odt(),
+                                       CGAL::parameters::no_perturb(), CGAL::parameters::no_exude());
 
-        C3t3_EIT c3t3;
-        c3t3 = CGAL::make_mesh_3<C3t3_EIT>(domain, criteria, CGAL::parameters::features(domain),
-        CGAL::parameters::no_lloyd(), CGAL::parameters::no_odt(),
-        CGAL::parameters::no_perturb(),CGAL::parameters::no_exude());
+    int vtk_success = write_c3t3_to_vtk_xml_file(c3t3, "cube_stretch" + to_string(deformations[i][0]) + to_string(deformations[i][1]) + to_string(deformations[i][2]) + ".vtu");
 
-        int vtk_success = write_c3t3_to_vtk_xml_file(c3t3, "cube_stretch" + to_string(deformations[i][0])
-                                                        + to_string(deformations[i][1])
-                                                         + to_string(deformations[i][2]) + ".vtu");
-
-        CHECK(vtk_success == 1);
-      }
-
-
+    CHECK(vtk_success == 1);
+  }
 }
