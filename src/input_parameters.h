d8e7eaf2bcc3bc4526fe1e3b39b5b5be491596b7
root <root@DESKTOP-7IQSD9M.localdomain>
2020-01-20 15:10:36 +0000

    Remove functions that are no longer needed

--- src/input_parameters.h
+++ src/input_parameters.h
@@ -7,7 +7,5 @@
 #include <sstream>
 
 std::map<std::string, FT> read_params_from_file(char *);
-std::vector<std::vector<double>> load_deformations(const char *);
-int check_distances_ok(double, double, double);
 
 #endif
