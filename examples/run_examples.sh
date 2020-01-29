93c2aae330640a429d7cc29c1270fc5a3cf1994d
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2020-01-29 11:44:10 +0000

    Update parameter files

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,8 +1,8 @@
-  - echo "Running Unit Cube Example"
-  - ../bin/mesher -i unitcube/unit_cube.inr -e unitcube/electrodes.txt  -p unitcube/input_idx.txt -o unitcubeexample -d unitcube/output/
+  echo "Running Unit Cube Example"
+  ../bin/mesher -i unitcube/unit_cube.inr -e unitcube/electrodes.txt  -p unitcube/Params_0.txt -o unitcubeexample -d unitcube/output/
 
-  - echo "Running Neonate Example"
-  - ../bin/mesher -i  neonatescalp/NNscalp.inr -e neonatescalp/NNscalp_elecINRpos.txt  -p neonatescalp/NNscalp_param.txt -o NNexample -d neonatescalp/output/
+  echo "Running Neonate Example"
+  ../bin/mesher -i  neonatescalp/NNscalp.inr -e neonatescalp/NNscalp_elecINRpos.txt  -p neonatescalp/NNscalp_param.txt -o NNexample -d neonatescalp/output/
 
-  - echo "Running Brain Example"
-  - ../bin/mesher -i brain/Brain.inr -e brain/Electrodes.txt -p brain/input_idx.txt -o brainexample -d brain/output/
+  echo "Running Brain Example"
+  ../bin/mesher -i brain/Brain.inr -e brain/Brain_elec.txt -p brain/Brain_params.txt -o brainexample -d brain/output/
