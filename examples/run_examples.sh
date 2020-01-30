3490e103e6314cef2a9de052562ffdf55b893c29
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2020-01-30 10:15:53 +0000

    Create output directories in example script

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -1,8 +1,11 @@
   echo "Running Unit Cube Example"
+  mkdir unitcube/output
   ../bin/mesher -i unitcube/unit_cube.inr -e unitcube/electrodes.txt  -p unitcube/Params_0.txt -o unitcubeexample -d unitcube/output/
 
   echo "Running Neonate Example"
+  mkdir neonatescalp/output
   ../bin/mesher -i  neonatescalp/NNscalp.inr -e neonatescalp/NNscalp_elecINRpos.txt  -p neonatescalp/NNscalp_param.txt -o NNexample -d neonatescalp/output/
 
   echo "Running Brain Example"
+  mkdir brain/output
   ../bin/mesher -i brain/Brain.inr -e brain/Brain_elec.txt -p brain/Brain_params.txt -o brainexample -d brain/output/
