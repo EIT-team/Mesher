87afb2d24c97e68b2682d741b15d0bb3aac178c1
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2020-02-10 14:50:03 +0000

    Fix refinment inr name

--- examples/run_examples.sh
+++ examples/run_examples.sh
@@ -15,9 +15,9 @@
   ../bin/mesher -i unitcube/unit_cube.inr -e unitcube/electrodes.txt -p deformation/deformation_params.txt -o unitcube_deformation -d deformation/output/
   
   echo "Running refinement Examples"
-  ../bin/mesher -i refinements/input.inr -e refinements/Elecs.txt -p refinements/Params_0.txt -d refinements/output/ -o 0_Initial
-  ../bin/mesher -i refinements/input.inr -e refinements/Elecs.txt -p refinements/Params_1.txt -d refinements/output/ -o 1_Elecs
-  ../bin/mesher -i refinements/input.inr -e refinements/Elecs.txt -p refinements/Params_2.txt -d refinements/output/ -o 2_Depth
-  ../bin/mesher -i refinements/input.inr -e refinements/Elecs.txt -p refinements/Params_3.txt -d refinements/output/ -o 3_Sphere
-  ../bin/mesher -i refinements/input.inr -e refinements/Elecs.txt -p refinements/Params_4.txt -d refinements/output/ -o 4_Cuboid
-  ../bin/mesher -i refinements/input.inr -e refinements/Elecs.txt -p refinements/Params_5.txt -d refinements/output/ -o 5_Planar
+  ../bin/mesher -i refinements/Input.inr -e refinements/Elecs.txt -p refinements/Params_0.txt -d refinements/output/ -o 0_Initial
+  ../bin/mesher -i refinements/Input.inr -e refinements/Elecs.txt -p refinements/Params_1.txt -d refinements/output/ -o 1_Elecs
+  ../bin/mesher -i refinements/Input.inr -e refinements/Elecs.txt -p refinements/Params_2.txt -d refinements/output/ -o 2_Depth
+  ../bin/mesher -i refinements/Input.inr -e refinements/Elecs.txt -p refinements/Params_3.txt -d refinements/output/ -o 3_Sphere
+  ../bin/mesher -i refinements/Input.inr -e refinements/Elecs.txt -p refinements/Params_4.txt -d refinements/output/ -o 4_Cuboid
+  ../bin/mesher -i refinements/Input.inr -e refinements/Elecs.txt -p refinements/Params_5.txt -d refinements/output/ -o 5_Planar
